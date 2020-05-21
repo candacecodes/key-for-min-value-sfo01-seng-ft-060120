044c592f0c8bfc4a8b5a5c83f3aaaba66dce8c37
eager-class-5814 <candacehchoe@gmail.com>
2020-05-21 03:49:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  final_key = ""
+  lowest_key = nil 
+  lowest_value = 0 
   name_hash.each do |key, value|
-    if final_key > value 
-      final_key = key
+    if lowest_key == 0 || value < lowest_value 
+      lowest_value = value
+      lowest_key = key
     end 
 end
+lowest_key
 end 
